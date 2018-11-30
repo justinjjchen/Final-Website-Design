6392c53758d7055c599d666cc573b2a629377957
justinjjchen <43192286+justinjjchen@users.noreply.github.com>
2018-11-30 18:51:12 -0500

    Add files via upload

---
+++ Final.js
@@ -0,0 +1,36 @@
+function topLane(){
+	document.getElementById("otherTier").innerHTML= "<strong> <br/> Tier 1: </strong> Garen, Jax, Teemo, Darius, Irelia, Yorick, Illaoi, Aatrox, Camille, Kayle, Renekton, Quinn, Wukong, Singed, Gangplank, Kled, Malphite, Poppy, Pantheon, Jarvan IV, Fiora, Jayce, Rumble, Lissandra, Riven, Akali <br/>" 
+	+ "<br/> <strong> Tier 2: </strong> Tryndamere, Swain, Volibear, Yasuo, Vladimir, Olaf, Nasus, Maokai, Shen, Gnar, Karma, Heimerdinger, Ornn, Cho'gath, Dr. Mundo, Malzahar, Kennen, Talon, Galio, Trundle, Gragas, Nautilus, Mordekaiser, Fizz, Ekko, Rammus, Rengar, Cassiopeia, Tahm Kench, Graves, Diana, Kassadin <br/>"	
+	+ "<br/> <strong> Tier 3: </strong> Udyr, Sejuani, Shaco, Zed, Ryze, Vayne, Lulu, Xin Zhao, Nunu, Rek'Sai, Karthus, Lee Sin, Kha'Zix, Vi, Shyvana <br/>"
+	+ "<br/> <strong> Tier 4: </strong> Blitzcrank, Taliyah, Alistar, Leblanc <br/>";
+	
+
+}
+
+function midLane(){
+	document.getElementById("otherTier").innerHTML= "<strong> <br/> Tier 1: </strong>Ahri, Malzahar, Twisted Fate, Fizz, Annie, Ekko, Anivia, Lissandra, Lux, Orianna, Talon, Veigar, Ziggs, Vel'Koz, Leblanc, Zed, Zilean, Irelia, Cassiopeia, Vladimir, Xerath <br/>" 
+	+ "<br/> <strong> Tier 2: </strong> Akali, Swain, Brand, Yasuo, Aurelion Sol, Aatrox, Wukong, Diana, Viktor, Karma, Zoe, Corki, Jayce, Azir, Gangplank, Syndra, Taliyah, Morgana, Galio, Kennen, Kayle, Gragas, Heimerdinger <br/>"	
+	+ "<br/> <strong> Tier 3: </strong> Ezreal, Lucian, Zyra, Ryze, Kog'Maw (AP), Cho'Gath, Pantheon, Quinn, Nasus, Lulu, Mordekaiser, Sona, Varus, Urgot, Teemo, Jarvan IV, Jhin, Riven, Sion, Fiddlesticks, Malphite <br/>"
+	+ "<br/> <strong> Tier 4: </strong> Master Yi, Janna, AP Kaisa <br/>";
+}
+
+function botLane(){
+	document.getElementById("otherTier").innerHTML= "<strong> <br/>Tier 1: </strong> Twitch, Ashe, Jinx, Draven, Xayah, Sivir, Ezreal, Kai'Sa, Vayne, Mordekaiser, Karthus, Quinn, Varus, Veigar, Ziggs <br/>" 
+	+ "<br/> <strong> Tier 2: </strong> Caitlyn, Tristana, Kog'Maw, Yasuo, Brand, Kalista, Swain, Jayce, Heimerdinger, Vladimir <br/>"	
+	+ "<br/> <strong> Tier 3: </strong> Kennen, Corki, Graves, Urgot <br/>"
+	+ "<br/> <strong> Tier 4: </strong> Twisted Fate <br/>";
+}
+
+function supLane(){
+	document.getElementById("otherTier").innerHTML= "<strong> <br/> Tier 1: </strong> Nami, Rakan, Brand, Alistar, Sona, Blitzcrank, Zyra, Soraka, Morgana, Pyke, Braum, Bard, Taric  <br/>" 
+	+ "<br/> <strong> Tier 2: </strong> Lulu, Lux, Vel'Koz, Shen, Fiddlesticks, Sion, Karma, Zilean, Nautilus, Xerath, Tahm Kench, Zoe, Maokai <br/>"	
+	+ "<br/> <strong> Tier 3: </strong> Poppy, Teemo, Malzahar, Galio, Cho'Gath, Camille, Trundle, Annie, Volibear, Malphite, Shaco, Veigar, Gragas, Nunu, Kayle, Leblanc, Kennen <br/>"
+	+ "<br/> <strong> Tier 4: </strong> Miss Fortune, Pantheon, Ashe, Anivia, Lee Sin, Gangplank, Orianna, Lissandra <br/>";
+}
+
+function jgLane(){
+	document.getElementById("otherTier").innerHTML= "<strong> <br/> Tier 1: </strong> Jax, Elise, Taliyah, Rammus, Evelynn, Camille, Kindred, Nocturne, Shaco, Nidalee, Ivern, Ekko, Udyr, Warwick, Kha'Zix, Nunu, Wukong, Rengar, Lee Sin <br/>" 
+	+ "<br/> <strong> Tier 2: </strong> Master Yi, Trundle, Maokai, Aatrox, Shyvana, Dr. Mundo, Skarner, Rek'Sai, Zac, Vi, Jarvan IV, Sejuani, Olaf, Cho'Gath, Hecarim, Gragas, Quinn, Amumu, Volibear, Pantheon, Karthus, Twitch <br/>"	
+	+ "<br/> <strong> Tier 3: </strong> Pyke, Poppy, Fizz, Galio, Kayle, Malphite, Sion, Nautilus, Diana, Rumble, Riven, Talon, Tryndamere, Kled, Mordekaiser, Irelia, Fiddlesticks, Tahm Kench, Kog'Maw <br/>"
+	+ "<br/> <strong> Tier 4: </strong> Nasus, Gangplank, Fiora, Shen, Darius, Zed, Aurelion Sol, Garen, Alistar, Yorick <br/>";
+}
